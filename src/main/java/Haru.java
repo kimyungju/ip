ec5c5f53b30aaa4e537c182331c2c39cd51f6ab0
Kim Yungju <yjkim101002@gmail.com>
2026-01-22 22:37:34 +0800

    Complete Level-0: rename Duke to Haru with greeting logo

---
+++ src/main/java/Haru.java
@@ -0,0 +1,15 @@
+public class Haru {
+    public static void main(String[] args) {
+        String logo =
+                  " _   _                 \n"
+                + "| | | | __ _ _ __ _   _ \n"
+                + "| |_| |/ _` | '__| | | |\n"
+                + "|  _  | (_| | |  | |_| |\n"
+                + "|_| |_|\\__,_|_|   \\__,_|\n";
+
+        System.out.println("Hello! I'm Haru");
+        System.out.println(logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
