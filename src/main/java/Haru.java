a269f815a012356182d1a8837d1ed2e34698a35b
Kim Yungju <yjkim101002@gmail.com>
2026-01-23 00:18:20 +0800

    Implement Level-3 mark and unmark

--- src/main/java/Haru.java
+++ src/main/java/Haru.java
@@ -1,6 +1,30 @@
 import java.util.Scanner;
 
 public class Haru {
+
+    private static class Task {
+        private String description;
+        private boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsNotDone() {
+            this.isDone = false;
+        }   
+
+        @Override
+        public String toString() {
+            return (isDone ? "[X] " : "[ ] ") + description;
+        }
+    }
+
     public static void main(String[] args) {
         String logo =
                   " _   _                 \n"
@@ -13,7 +37,7 @@ public class Haru {
         System.out.println(logo);
         System.out.println("What can I do for you?");
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         Scanner sc = new Scanner(System.in);    
@@ -26,15 +50,34 @@ public class Haru {
             }
 
             if (line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
-            } else {
-                tasks[taskCount] = line;
-                taskCount++;
-                System.out.println("added: " + line);
+                continue;
+            } 
+            
+            if (line.startsWith("mark ")) {
+                int index = Integer.parseInt(line.substring(5)) - 1;
+                tasks[index].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[index]);
+                continue;
             }
+
+            if (line.startsWith("unmark ")) {
+                int index = Integer.parseInt(line.substring(7)) - 1;
+                tasks[index].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[index]);
+                continue;
+            }
+                
+            tasks[taskCount] = new Task(line);
+            taskCount++;
+            System.out.println("added: " + line);
         }
+
         sc.close();
     }
 }
