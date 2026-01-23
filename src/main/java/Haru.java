64593702107895819ea4833476fd1cf618f11ff6
Kim Yungju <yjkim101002@gmail.com>
2026-01-23 18:45:36 +0800

    Implement Level-6: Delete and ArrayList

--- src/main/java/Haru.java
+++ src/main/java/Haru.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner; 
 
 public class Haru {
     public static void main(String[] args) {
@@ -6,15 +7,14 @@ public class Haru {
                   " _   _                 \n"
                 + "| | | | __ _ _ __ _   _ \n"
                 + "| |_| |/ _` | '__| | | |\n"
-                + "|  _  | (_| | |  | |_| |\n"
+                + "|  _  | (_| | |   | |_| |\n"
                 + "|_| |_|\\__,_|_|   \\__,_|\n";
 
         System.out.println("Hello! I'm Haru");
         System.out.println(logo);
         System.out.println("What can I do for you?");
 
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>(); 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -22,89 +22,83 @@ public class Haru {
 
             try {
                 if (line.equals("bye")) {
-                    System.out.println("    ____________________________________________________________");
+                    printLine();
                     System.out.println("     Bye. Hope to see you again soon!");
-                    System.out.println("    ____________________________________________________________");
+                    printLine();
                     break;
                 }
 
                 if (line.equals("list")) {
-                    System.out.println("    ____________________________________________________________");
+                    printLine();
                     System.out.println("     Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println("     " + (i + 1) + "." + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++) { 
+                        System.out.println("     " + (i + 1) + "." + tasks.get(i)); 
                     }
-                    System.out.println("    ____________________________________________________________");
+                    printLine();
                 } else if (line.startsWith("mark")) {
-                    if (line.length() <= 5) {
-                        throw new HaruException("Which task should I mark? Please provide a number.");
-                    }
-                    int index = Integer.parseInt(line.substring(5)) - 1;
-                    if (index < 0 || index >= taskCount) {
-                        throw new HaruException("That task number doesn't exist in your list.");
-                    }
-                    tasks[index].markAsDone();
-                    System.out.println("    ____________________________________________________________");
-                    System.out.println("     Nice! I've marked this task as done:\n       " + tasks[index]);
-                    System.out.println("    ____________________________________________________________");
+                    int index = getIndex(line, 5, tasks.size());
+                    tasks.get(index).markAsDone();
+                    printLine();
+                    System.out.println("     Nice! I've marked this task as done:\n       " + tasks.get(index));
+                    printLine();
                 } else if (line.startsWith("unmark")) {
-                    if (line.length() <= 7) {
-                        throw new HaruException("Which task should I unmark? Please provide a number.");
-                    }
-                    int index = Integer.parseInt(line.substring(7)) - 1;
-                    if (index < 0 || index >= taskCount) {
-                        throw new HaruException("I couldn't find that task number.");
-                    }
-                    tasks[index].markAsNotDone();
-                    System.out.println("    ____________________________________________________________");
-                    System.out.println("     OK, I've marked this task as not done yet:\n       " + tasks[index]);
-                    System.out.println("    ____________________________________________________________");
+                    int index = getIndex(line, 7, tasks.size());
+                    tasks.get(index).markAsNotDone();
+                    printLine();
+                    System.out.println("     OK, I've marked this task as not done yet:\n       " + tasks.get(index));
+                    printLine();
+                } else if (line.startsWith("delete")) {
+                    int index = getIndex(line, 7, tasks.size());
+                    Task removedTask = tasks.remove(index); 
+                    printLine();
+                    System.out.println("     Noted. I've removed this task:\n       " + removedTask);
+                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                    printLine();
                 } else if (line.startsWith("todo")) {
-                    if (line.length() <= 5) {
-                        throw new HaruException("The description of a todo cannot be empty.");
-                    }
-                    tasks[taskCount++] = new Todo(line.substring(5));
-                    printAddedMessage(tasks[taskCount - 1], taskCount);
+                    if (line.length() <= 5) throw new HaruException("The description of a todo cannot be empty.");
+                    tasks.add(new Todo(line.substring(5))); 
+                    printAddedMessage(tasks.get(tasks.size() - 1), tasks.size());
                 } else if (line.startsWith("deadline")) {
-                    if (!line.contains(" /by ")) {
-                        throw new HaruException("A deadline needs a time! Use: deadline [desc] /by [time]");
-                    }
+                    if (!line.contains(" /by ")) throw new HaruException("A deadline needs a time! Use: /by");
                     String[] parts = line.substring(9).split(" /by ");
-                    if (parts[0].trim().isEmpty()) {
-                        throw new HaruException("A deadline needs a description.");
-                    }
-                    tasks[taskCount++] = new Deadline(parts[0], parts[1]);
-                    printAddedMessage(tasks[taskCount - 1], taskCount);
+                    tasks.add(new Deadline(parts[0], parts[1]));
+                    printAddedMessage(tasks.get(tasks.size() - 1), tasks.size());
                 } else if (line.startsWith("event")) {
-                    if (!line.contains(" /from ") || !line.contains(" /to ")) {
-                        throw new HaruException("Events need /from and /to times.");
-                    }
+                    if (!line.contains(" /from ") || !line.contains(" /to ")) throw new HaruException("Events need /from and /to.");
                     String[] parts = line.substring(6).split(" /from | /to ");
-                    if (parts[0].trim().isEmpty()) {
-                        throw new HaruException("An event needs a description.");
-                    }
-                    tasks[taskCount++] = new Event(parts[0], parts[1], parts[2]);
-                    printAddedMessage(tasks[taskCount - 1], taskCount);
+                    tasks.add(new Event(parts[0], parts[1], parts[2]));
+                    printAddedMessage(tasks.get(tasks.size() - 1), tasks.size());
                 } else {
                     throw new HaruException("I'm sorry, but I don't know what that means :-(");
                 }
             } catch (HaruException e) {
-                System.out.println("    ____________________________________________________________");
+                printLine();
                 System.out.println("     OOPS!!! " + e.getMessage());
-                System.out.println("    ____________________________________________________________");
+                printLine();
             } catch (NumberFormatException e) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     OOPS!!! Please use a valid number for marking.");
-                System.out.println("    ____________________________________________________________");
+                printLine();
+                System.out.println("     OOPS!!! Please use a valid number.");
+                printLine();
             }
         }
         sc.close();
     }
 
+    private static int getIndex(String line, int offset, int size) throws HaruException {
+        if (line.length() <= offset) throw new HaruException("Please provide a task number.");
+        int index = Integer.parseInt(line.substring(offset).trim()) - 1;
+        if (index < 0 || index >= size) throw new HaruException("That task number doesn't exist.");
+        return index;
+    }
+
     private static void printAddedMessage(Task task, int count) {
-        System.out.println("    ____________________________________________________________");
+        printLine();
         System.out.println("     Got it. I've added this task:\n       " + task);
         System.out.println("     Now you have " + count + " tasks in the list.");
+        printLine();
+    }
+
+    private static void printLine() {
         System.out.println("    ____________________________________________________________");
     }
 }
